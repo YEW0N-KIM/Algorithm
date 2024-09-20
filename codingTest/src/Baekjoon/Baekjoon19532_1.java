e9dfd0f5a59a01bbcface07bb314ed0253b3d5fa
yewon_kim <wonwhimsical@gmail.com>
2024-09-20 23:13:05 +0900

    add Baekjoon 240920

--- codingTest/src/Baekjoon/Baekjoon19532_1.java
+++ codingTest/src/Baekjoon/Baekjoon19532_1.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon19532 {
+public class Baekjoon19532_1 {
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
